352e6cf03e9ba39362e7beb99a0317d5352640e5
yuhaibo <yuwenhaibo1@gmail.com>
2025-01-30 23:05:57 +0800

    update

--- api/index.js
+++ api/index.js
@@ -1,21 +1,16 @@
-const fs = require("fs");
-const path = require("path");
+const { readFileSync } = require("fs");
+const { join } = require("path");
 
-module.exports = async (req, res) => {
-  try {
-    // 修正文件路径：使用 __dirname 直接指向当前 API 目录
-    const filePath = path.join(__dirname, "data.json");
-
-    // 确保文件存在
-    if (!fs.existsSync(filePath)) {
-      throw new Error("JSON file not found");
-    }
-
-    const jsonData = fs.readFileSync(filePath, "utf8");
+module.exports = (req, res) => {
+  if (req.url === "/iptv4.txt") {
+    const iptvData = readFileSync(join(__dirname, "iptv4.txt"), "utf-8");
+    res.setHeader("Content-Type", "text/plain; charset=utf-8");
+    res.statusCode = 200;
+    res.end(iptvData);
+  } else {
+    const data = readFileSync(join(__dirname, "data.json"), "utf-8");
     res.setHeader("Content-Type", "application/json");
-    res.status(200).json(JSON.parse(jsonData));
-  } catch (error) {
-    console.error("Error:", error.message);
-    res.status(500).json({ error: error.message });
+    res.statusCode = 200;
+    res.end(data);
   }
 };
